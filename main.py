906a482d07c0aae27b01dcafec6ea81d660fec50
EderChri <ce1@live.de>
2023-05-16 11:14:40 +0200

    Changes main.py     WIP implementation of the plotting and the information spread

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from cityvillage import City, Village, CityVillageGraph
 import igraph
 import random
+import matplotlib.pyplot as plt
 
 random.seed(42)
 
@@ -10,11 +11,17 @@ def run_simulation():
     city = City(number_nodes=40)
     graph = CityVillageGraph()
     graph = graph.add_locations(city, villages)
-    out = igraph.plot(graph.get_igraph_representation(), target='test.png',
-                      bbox=(800, 800),
-                      margin=50,
-                      vertex_label=None, edge_width=1, edge_color='black')
-    out.save("test.png")
+    colormap = plt.cm.viridis
+
+    while not graph.all_infected():
+        not_interested, spreading, ignorant = graph.spread_information()
+        node_colors = [colormap(value) for value in ["spreading", "not_interested", "ignorant"]]
+        out = igraph.plot(graph.get_igraph_representation(), target='test.png',
+                          bbox=(800, 800),
+                          margin=50,
+                          vertex_label=None, edge_width=1, edge_color='black',
+                          vertex_color=node_colors)
+        out.save("test.png")
 
 
 if __name__ == '__main__':
