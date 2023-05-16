989d13c5ff89a74d2d5c59c16050cea1231d9a49
Lindsey de Beer <lindsey.debeer@ru.nl>
2023-05-16 14:01:26 +0200

    add line plot

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ import imageio
 random.seed(42)
 
 
-
-
 def run_simulation():
     city_size = 5
     village_size = 3
@@ -23,7 +21,8 @@ def run_simulation():
     startpoint = city_size + random.randint(0, village_size*nr_villages) - 1
     graph.vs[startpoint]['state'] = 'spreading'
     not_interested_counts = [] 
-
+    spreading_counts = [] 
+    ignorant_counts = [] 
     count = 0 #used to debug while loop
     while not graph.all_informed():
         node_colors = [colormap[state] for state in graph.vs['state']]
@@ -33,21 +32,26 @@ def run_simulation():
                           vertex_label=None, edge_width=1, edge_color='black',
                           vertex_color=node_colors)
         out.save(f"img/graph_{count}.png")
-        not_interested_counts.append(not_interested) 
 
         #used to debug while loop -> overall works, but sometimes error incident function gives non existent node which is larger than the amount of nodes and not a neighbour
         print(graph.vs['state'])
         count = count + 1
         not_interested, spreading, ignorant = graph.spread_information()
+        not_interested_counts.append(not_interested) 
+        spreading_counts.append(spreading)
+        ignorant_counts.append(ignorant)
 
-        plt.figure(figsize=(10, 6))
-        plt.plot(not_interested_counts, label='Not Interested')
-        plt.title('Number of Not Interested Nodes Over Time')
-        plt.xlabel('Iteration')
-        plt.ylabel('Number of Not Interested Nodes')
-        plt.legend()
-        plt.grid(True)
-        plt.savefig('output.png')
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(ignorant_counts, label='Ignorant')
+    plt.plot(spreading_counts, label='Spreading')
+    plt.plot(not_interested_counts, label='Not Interested')
+    plt.title('Number Of Nodes For Each State Over Time')
+    plt.xlabel('Iteration')
+    plt.ylabel('Number of Nodes')
+    plt.legend()
+    plt.grid(True)
+    plt.savefig('output.png')
     
 if __name__ == '__main__':
     run_simulation()
