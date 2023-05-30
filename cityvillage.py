0f46a19af6aba594894a70391f274fb2c7373c25
EderChri <ce1@live.de>
2023-05-30 16:55:17 +0200

    Changes in cityvillage.py     Added documentation, mostly in form of docstrings
    Changes in main.py
        Added documenation, mostly in form of docstrings
        Refactored variables that did not follow coding conventions
        Added simulation pipeline that runs all the experiments
        Added necessary class (Experiments) and functions (get_row_dict, sim_wrapper, print_header)
    
    Added spreading.csv
        File of the initial experiment run

--- cityvillage.py
+++ cityvillage.py
@@ -3,7 +3,18 @@ from igraph import Graph
 
 
 class Dwelling(Graph):
+    """
+    Class to represent either a village or a city
+    """
+
     def __init__(self, number_nodes, prob, *args, **kwds):
+        """
+        Constructor that adds a number_nodes nodes to the graph and connects them with prob probability
+        :param number_nodes: Int that indicates the number of nodes in the Dwelling
+        :param prob: Float that indicates the probability of two nodes being connected by an edge in the Dwelling
+        :param args: Args to be passed to super
+        :param kwds: kwds to be passed to super
+        """
         super().__init__(*args, **kwds)
         for _ in range(number_nodes):
             self.add_vertex()
@@ -13,10 +24,22 @@ class Dwelling(Graph):
 
 
 class CityVillageGraph(Graph):
+    """
+    Class to represent graph of network of city and villages
+    """
     villages = []
     city = None
 
     def __init__(self, number_connections=3, spreading_time=5, time_out=True, *args, **kwds):
+        """
+        Constructor to set additional properties
+        :param number_connections: Int to indicate the mean number of connections between the city and a village
+        :param spreading_time: Int indicating the time a node spreads if in spreading state and neighbours are in
+            ignorant state
+        :param time_out: Boolean indicating whether nodes stop spreading after some iterations or not
+        :param args: args to be passed to super
+        :param kwds: kwds to be passed to super
+        """
         super().__init__(*args, **kwds)
         self.nr_connections = number_connections
         self.time_out = time_out
@@ -25,8 +48,11 @@ class CityVillageGraph(Graph):
         self.spreading_time = spreading_time
 
     def __add__(self, other):
-        # Needed so the igraph.Graph addition works
-        # But the class properties are kept as well
+        """
+        Overwritting the default __add__ of igraph.Graph __add__ to keep class properties
+        :param other:
+        :return: self
+        """
         tmp_vil = self.villages
         tmp_city = self.city
         tmp_nr_con = self.nr_connections
@@ -40,7 +66,13 @@ class CityVillageGraph(Graph):
         self.time_out = tmp_time_out
         return self
 
-    def add_locations(self, city, villages):
+    def add_dwellings(self, city, villages):
+        """
+        Adds a city and a list of villages to the graph
+        :param city: Dwelling object that represents the city
+        :param villages: List of Dwelling objects that represent the villages
+        :return: self
+        """
         # If city has not been added yet, add city and
         # connections between city and village
         filename = './output.txt'
@@ -67,21 +99,34 @@ class CityVillageGraph(Graph):
                     print(self, file=f)
             idx = idx + village.vcount()
 
+        # Set initial properties of nodes
         self.vs["state"] = "ignorant"
         self.vs["action"] = False
         self.vs["time"] = self.spreading_time
         return self
 
     def make_complete_graph(self):
+        """
+        Helper function to make the graph complete i.e, every node is connected with every other node in the graph
+        :return:
+        """
         self.add_edges([(i, j) for i in range(self.vcount()) for j in range(i + 1, self.vcount())])
 
     def get_igraph_representation(self):
+        """
+        Helper function to convert the VillageCityGraph to an igraph object. Used for plotting
+        :return: igraph.Graph representation of self
+        """
         igraph = Graph()
         igraph.add_vertices(self.vcount())
         igraph.add_edges(self.get_edgelist())
         return igraph
 
     def not_spreading(self):
+        """
+        Checks whether all nodes stopped spreading in the graph
+        :return: Boolean
+        """
         if all(i != 'spreading' for i in self.vs['state']):
             not_spreading = True
         else:
@@ -89,7 +134,13 @@ class CityVillageGraph(Graph):
         return not_spreading
 
     def spread_information(self, nr_not_interested=0, nr_spreading=0, spread_prob=0.4):
-
+        """
+        Method used to run one iteration of spreading information.
+        :param nr_not_interested: Int indicating the number of not_interested nodes at the beginning of the iteration
+        :param nr_spreading: Int indicating the number of spreading nodes at the beginning of the iteration
+        :param spread_prob: Float indicating the spreading probability for this iteration
+        :return: List of integers indicating the number of not_interested, spreading and ignorant nodes after iteration
+        """
         self.vs["action"] = False
 
         for node_idx in self.vs.indices:
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import copy
 import os.path
 from dataclasses import dataclass
 
@@ -11,10 +12,16 @@ from os import listdir
 from os.path import isfile, join
 import glob
 from pathlib import Path
+import pandas as pd
+from tqdm import tqdm
 
 
 @dataclass
 class SimSettings:
+    """
+    Data class used for the general settings of the simulation
+    Here the baseline parameter should be set
+    """
     city_size = 40
     village_size = 10
     nr_villages = 5
@@ -22,7 +29,7 @@ class SimSettings:
     time_out = False
     decay = True
     spreading_time = 2
-    num_startpoints = 5
+    num_start_points = 5
     only_villages = False
     only_cities = False
     seed = 42
@@ -30,78 +37,117 @@ class SimSettings:
     connect_prob_vil = 0.5
 
 
-def run_simulation(cfg: SimSettings):
-    villages = [Dwelling(number_nodes=cfg.village_size, prob=cfg.connect_prob_vil)
-                for _ in range(cfg.nr_villages)]
-    city = Dwelling(number_nodes=cfg.city_size, prob=cfg.connect_prob_city)
-    graph = CityVillageGraph(time_out=cfg.time_out, spreading_time=cfg.spreading_time)
-    graph = graph.add_locations(city, villages)
+@dataclass
+class Experiments:
+    """
+    Data class for the parameter ranges run through in the experiments
+    """
+    spreading_prob = [0.3, 0.6, 0.9]
+    spreading_time = [0, 3, 5]
+    connect_prob_city = [0.25, 0.75, 1]
+    connect_prob_vil = [0.25, 0.75, 1]
+    num_start_points = [1, 5]
+
+
+def run_simulation(config: SimSettings, plot=False):
+    """
+    Main class for the simulation. Performs one run of the simulation given the parameter
+    :param config: SimSettings object that determines all parameters of the simulation
+    :param plot: Boolean that defines whether the run should be plotted or not
+    :return:
+    """
+    # Create villages, city and the graph
+    villages = [Dwelling(number_nodes=config.village_size, prob=config.connect_prob_vil)
+                for _ in range(config.nr_villages)]
+    city = Dwelling(number_nodes=config.city_size, prob=config.connect_prob_city)
+    graph = CityVillageGraph(time_out=config.time_out, spreading_time=config.spreading_time)
+    graph = graph.add_dwellings(city, villages)
+    # Defines colormap for nodes in plot
     colormap = {"spreading": "red", "not_interested": "blue", "ignorant": "yellow"}
 
-    # create startpoints
+    # create start_points
     startpoint_loc = []
-    startpoints = []
-    # make list of whether startpoints are in city or village if there are multiple points
-    for i in range(cfg.num_startpoints):
-        if cfg.only_villages:
+    start_points = []
+    # make list of whether start_points are in city or village if there are multiple points
+    for i in range(config.num_start_points):
+        if config.only_villages:
             startpoint_loc.append(True)
-        elif cfg.only_cities:
+        elif config.only_cities:
             startpoint_loc.append(False)
         else:
             # control that the same value is not taken all the time
-            np.random.seed(cfg.seed + i)
+            np.random.seed(config.seed + i)
             startpoint_loc.append(np.random.choice([True, False]))
 
-    # get startpoints based on locations
-    for i in range(cfg.num_startpoints):
-        np.random.seed(cfg.seed + i)
+    # get start_points based on locations
+    for i in range(config.num_start_points):
+        np.random.seed(config.seed + i)
         if startpoint_loc[i]:
             # WARNING make sure that the city and village size is larger than the amount of starting points
             # check if startpoint is already present
             while True:
-                if (cfg.city_size + np.random.randint(0, cfg.village_size * cfg.nr_villages)) not in startpoints:
-                    startpoints.append(cfg.city_size + np.random.randint(0, cfg.village_size * cfg.nr_villages))
+                if (config.city_size + np.random.randint(0,
+                                                         config.village_size * config.nr_villages)) not in start_points:
+                    start_points.append(
+                        config.city_size + np.random.randint(0, config.village_size * config.nr_villages))
                     break
         else:
             while True:
-                if np.random.randint(0, cfg.city_size) not in startpoints:
-                    startpoints.append(np.random.randint(0, cfg.city_size))
+                if np.random.randint(0, config.city_size) not in start_points:
+                    start_points.append(np.random.randint(0, config.city_size))
                     break
 
-    for startpoint in startpoints:
+    for startpoint in start_points:
         graph.vs[startpoint]['state'] = 'spreading'
 
+    # used for counting nodes of different states
     not_interested_counts = [0]
-    spreading_counts = [cfg.num_startpoints]
-    ignorant_counts = [cfg.nr_villages * cfg.village_size + cfg.city_size - cfg.num_startpoints]
-    count = 0  # for naming the graph images
+    spreading_counts = [config.num_start_points]
+    ignorant_counts = [config.nr_villages * config.village_size + config.city_size - config.num_start_points]
+    # Defines the number of iterations
+    count = 0
     not_interested = 0
-    spreading = cfg.num_startpoints
+    spreading = config.num_start_points
+    spreading_prob = config.spreading_prob
     while not graph.not_spreading() and count < 30:
         plot_graph(graph, count, colormap)
         count = count + 1
         not_interested, spreading, ignorant = graph.spread_information(nr_not_interested=not_interested,
                                                                        nr_spreading=spreading,
-                                                                       spread_prob=cfg.spreading_prob)
-        if cfg.decay:
-            cfg.spreading_prob = cfg.spreading_prob * np.exp(-0.075 * count)
+                                                                       spread_prob=spreading_prob)
+        if config.decay:
+            spreading_prob = spreading_prob * np.exp(-0.075 * count)
         not_interested_counts.append(not_interested)
         spreading_counts.append(spreading)
         ignorant_counts.append(ignorant)
-    plot_graph(graph, count, colormap)
-    plot_statistics(not_interested_counts, spreading_counts, ignorant_counts)
+
+    if plot:
+        plot_graph(graph, count, colormap)
+        plot_statistics(not_interested_counts, spreading_counts, ignorant_counts)
+    return count
 
 
 def generate_gif():
+    """
+    Generates a gif out of the images in the img dir
+    :return:
+    """
     filenames = [f for f in listdir('img') if isfile(join('img', f))]
     filenames.sort(key=lambda x: os.path.getmtime(os.path.join("img", x)))
     images = []
     for filename in filenames:
         images.append(imageio.v2.imread(os.path.join('img', filename)))
-    imageio.mimsave('information_spread.gif', images, duration=1000, format="GIF")
+    imageio.mimsave('information_spread.gif', images, duration=1000, format="GIF", loop=0)
 
 
 def plot_statistics(not_interested_counts, spreading_counts, ignorant_counts):
+    """
+    Plots the number of nodes of certain states against iterations
+    :param not_interested_counts: List of nodes in the not_interested state at given iteration
+    :param spreading_counts: List of nodes in the spreading state at given iteration
+    :param ignorant_counts: List of nodes in the ignorant state at given iteration
+    :return:
+    """
     plt.figure(figsize=(10, 6))
     plt.plot(ignorant_counts, label='Ignorant', c='yellow')
     plt.plot(spreading_counts, label='Spreading', c='red')
@@ -115,6 +161,13 @@ def plot_statistics(not_interested_counts, spreading_counts, ignorant_counts):
 
 
 def plot_graph(graph, number, colormap):
+    """
+    Generates a plot of the cityvillage graph
+    :param graph: CityVillageGraph object to be plotted
+    :param number: Int indicating the iteration, used for naming
+    :param colormap: Dictionary indicating colour for each node
+    :return:
+    """
     fig, ax = plt.subplots()
     node_colors = [colormap[state] for state in graph.vs['state']]
     igraph.plot(graph.get_igraph_representation(),
@@ -127,15 +180,84 @@ def plot_graph(graph, number, colormap):
 
 
 def cleanup_directory():
+    """
+    Function that removes all png from the img folder
+    :return:
+    """
     Path("img").mkdir(parents=True, exist_ok=True)
     filelist = glob.glob(os.path.join('img', "*.png"))
     for f in filelist:
         os.remove(f)
 
 
+def get_row_dict(config, time):
+    """
+    Helper function to generate a dictionary row
+    :param config: SimSettings object containing all the parameter
+    :param time: Int indicating the iteration
+    :return: Dictionary used for pandas DataFrame generation
+    """
+    row = {"time": time, "spreading_prob": config.spreading_prob, "time_out": config.time_out, "decay": config.decay,
+           "connect_prob_city": config.connect_prob_city, "connect_prob_vil": config.connect_prob_vil,
+           "num_start_points": config.num_start_points, "only_cities": config.only_cities,
+           "only_villages": config.only_villages, "spreading_time": config.spreading_time}
+    return row
+
+
+def sim_wrapper(config, return_list, change_parameter, param_name):
+    """
+    Wrapper function, that runs the simulation using the given experiment parameters
+    :param config: SimSettings object that contains the baseline parameters
+    :param return_list: List of dictionaries used for pandas DataFrame generation
+    :param change_parameter: List of values for the parameter that is varied in this experiment
+    :param param_name: String containing the name of the parameter to be varied in this experiment
+    :return: List of dictionaries with the outcome of this experiment added
+    """
+    sim_cfg = copy.deepcopy(config)
+    for param in tqdm(change_parameter):
+        setattr(sim_cfg, param_name, param)
+        time = run_simulation(sim_cfg)
+        return_list.append(get_row_dict(sim_cfg, time))
+    return return_list
+
+
+def print_header(header):
+    """
+    Short helper function to print nice header output
+    :param header: String that is to be printed with a header
+    :return:
+    """
+    print('-' * 50)
+    print(header)
+
+
 if __name__ == '__main__':
-    cfg = SimSettings()
-    np.random.seed(cfg.seed)
+    sim_list = []
     cleanup_directory()
-    run_simulation(cfg)
-    generate_gif()
+    cfg = SimSettings()
+    exp = Experiments()
+    np.random.seed(cfg.seed)
+    # Run all the settings for all different numbers of starting points
+    for num_start_points in exp.num_start_points:
+        cfg.num_start_points = num_start_points
+        print('=' * 50)
+        print(f'{num_start_points} Start point(s):')
+        print_header("Experiment spreading_prob with decay")
+        sim_list = sim_wrapper(cfg, sim_list, exp.spreading_prob, "spreading_prob")
+        print_header("Experiment connect_prob_city with decay")
+        sim_list = sim_wrapper(cfg, sim_list, exp.connect_prob_city, "connect_prob_city")
+        print_header("Experiment connect_prob_vil with decay")
+        sim_list = sim_wrapper(cfg, sim_list, exp.connect_prob_vil, "connect_prob_vil")
+        # Switch from decay to time_out
+        cfg.decay = False
+        cfg.time_out = True
+        print_header("Experiment spreading_prob with time_out")
+        sim_list = sim_wrapper(cfg, sim_list, exp.spreading_prob, "spreading_prob")
+        print_header("Experiment connect_prob_city with time_out")
+        sim_list = sim_wrapper(cfg, sim_list, exp.connect_prob_city, "connect_prob_city")
+        print_header("Experiment connect_prob_vil with time_out")
+        sim_list = sim_wrapper(cfg, sim_list, exp.connect_prob_vil, "connect_prob_vil")
+        print_header("Experiment spreading_time with time_out")
+        sim_list = sim_wrapper(cfg, sim_list, exp.spreading_time, "spreading_time")
+    df = pd.DataFrame(sim_list)
+    df.to_csv("spreading.csv", index=False)
